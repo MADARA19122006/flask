45228ceecdb317fb92c51c5a1ba1fb84f5c8ef17
MADARA19122006 <nikolayb1206@gmail.com>
2021-03-06 18:18:46 +0300

    vsdv

--- Тренировки в полёте.py
+++ Тренировки в полёте.py
@@ -5,14 +5,11 @@ app = Flask(__name__)
 
 @app.route('/training/<prof>')
 def index(prof):
-    title1 = "Миссия Колонизация Марса"
-    title2 = "И на Марсе будут яблони цвести!"
     if prof == 'инженер' or prof == 'строитель':
         a = 'Инженерные тренажеры'
     else:
         a = 'Научные симуляторы'
-    return render_template('Mars.html', title=prof, title1=title1,
-                           title2=title2, workout=a,
+    return render_template('Mars.html', title=prof, workout=a,
                            picture=url_for('static', filename='img/MARS2.png'))
 
 
